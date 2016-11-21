11586ebb28e5232571903619fbc8146e37c9bf56
Niko <niko.nyrhila@gmail.com>
2016-11-21 12:11:42 +0200

    Added kibana4, updated startKibanaContainer.sh

--- startKibanaContainer.sh
+++ startKibanaContainer.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-IP=`./ip.sh`
+if [ "$ELASTICSEARCH_URL" = "" ]; then
+    IP=`./ip.sh`
+    ELASTICSEARCH_URL="http://$IP:9200"
+fi
+
 docker run -p 5601:5601 \
-    -e ELASTICSEARCH_URL="http://$IP:9200" \
-    -d kibana
+    -e ELASTICSEARCH_URL=$ELASTICSEARCH_URL \
+    -d "kibana$1"
+
+# Let's give Kibana some time to create the index...
+sleep 5
+
+curl -XPUT "http://$ELASTICSEARCH_URL/.kibana/_settings" -d '{"index": {"number_of_replicas": 0}}'
