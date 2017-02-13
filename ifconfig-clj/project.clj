78d3bc1b6ffb3d29119111351c273c0de244d619
nikonyrh <niko.nyrhila@gmail.com>
2017-02-13 19:39:35 +0200

    ifconfig-clj lambda example, not yet fully implemented as need to integrate with AWS API Gateway

--- ifconfig-clj/project.clj
+++ ifconfig-clj/project.clj
@@ -2,9 +2,11 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.6"]
-                 [ring/ring-core "1.5.1"]]
+                 [ring/ring-core "1.5.1"]
+                 [uswitch/lambada "0.1.2"]]
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
+  :java-source-paths ["src/java"]
   :aot [ifconfig-clj.core]
   :main ifconfig-clj.core
   :plugins [[lein-ring "0.11.0"]]
-  :ring {:handler ifconfig-clj.core/handler})
+  :ring {:handler ifconfig-clj.core/-ring-handler})
--- ifconfig-clj/src/ifconfig_clj/core.clj
+++ ifconfig-clj/src/ifconfig_clj/core.clj
@@ -1,14 +1,22 @@
 (ns ifconfig-clj.core
-  (:require [clojure.data.json :as json])
-  (:gen-class))
+  (:require [clojure.data.json :as json]
+            [clojure.java.io :as io]
+            [uswitch.lambada.core :refer [deflambdafn]]))
 
-; A very basic JSON response with some ad-hoc pretty printing.
-(defn handler [request]
+
+(defn handle-lambda [event]
+  (println "Got the following event: " (pr-str event))
+  {:status "ok"})
+
+
+(deflambdafn ifconfig.lambda [in out ctx]
+  (let [event (-> in io/reader json/read)
+        res (handle-lambda event)]
+    (with-open [w (io/writer out)]
+      (json/write res w))))
+
+
+(defn -ring-handler [request]
   {:status 200
-   :headers {"Content-Type" "application/json"}
-   :body (-> {:remote-addr (:remote-addr request)
-              :user-agent ((:headers request) "user-agent")}
-             json/write-str
-             (clojure.string/replace #"\",\"" "\",\n  \"")
-             (clojure.string/replace #"\":\"" "\": \"")
-             (clojure.string/replace #"^\{\"" "{ \""))})
+   :headers {"Content-Type" "text/plain"}
+   :body (str (:remote-addr request) "\n")})
