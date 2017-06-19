fecb5a20db6005f480b484eb0a191c2190697c65
Niko <niko.nyrhila@gmail.com>
2017-06-19 18:03:52 +0300

    Fixed Docker installation on AWS Ubuntu Xenial with kernel 4.4.0

--- getDockerUbuntu.sh
+++ getDockerUbuntu.sh
@@ -12,17 +12,24 @@ if [ "$DOCKER" != "" ]; then
    >&2 echo -e "\n\nDokcer already installed!\n\n" && exit 0
 fi
 
-# The kernel version
-UNAME=`uname -r`
+# ref. https://github.com/docker/docker.github.io/issues/2694
+if [ `grep aufsz /proc/filesystems` = "" ]; then
+    # linux-image-extra is needed for AUFS support on older kernels.
+    UNAME=`uname -r`
+    KERNEL_IMAGE_EXTRA="apt-get install -y 'linux-image-extra-$UNAME'"
+else
+    # AUFS seems to be already supported
+    KERNEL_IMAGE_EXTRA='echo "Skipping installing linux-image-extra, should not be needed"'
+fi
 
-# Has values like ubuntu-trhusty and ubuntu-xenial
+# $UBUNTU values like ubuntu-trhusty and ubuntu-xenial
 UBUNTU=`lsb_release -c | sed -r 's/.+[ \t]/ubuntu-/'`
 
 # Based on https://docs.docker.com/engine/installation/ubuntulinux/
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D && \
 	echo "deb https://apt.dockerproject.org/repo $UBUNTU main" > /etc/apt/sources.list.d/docker.list && \
 	apt-get update && \
-	apt-get install -y "linux-image-extra-$UNAME" && \
+	$KERNEL_IMAGE_EXTRA && \
 	apt-get install -y docker-engine && \
 	echo "Install ok, setting user group" && \
 	service docker start 2>/dev/null
@@ -34,6 +41,6 @@ fi
 
 getent passwd ubuntu >/dev/null 2>&1
 if [[ $? -eq 0 ]]; then
-    # Add "ubuntu" user to the docker group, should auto-detect the username
+    # Add "ubuntu" user to the docker group
     usermod -aG docker ubuntu
 fi
