367c1e66e652011630986afb82ce503d98fad2bb
nikonyrh <niko.nyrhila@gmail.com>
2017-09-11 22:31:30 +0300

    Upgraded Redis 3 to 4

--- redis/main.sh
+++ redis/main.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-REDIS_MEM=`/redisMemory.sh`
-sed -i -r "s/(maxmemory ).+/\1$REDIS_MEM/" /usr/local/etc/redis/redis.conf && \
-    redis-server /usr/local/etc/redis/redis.conf
+
+if [ "$2" == "" ]; then
+    >&2 echo "Memory policies: noeviction allkeys-lru volatile-lru allkeys-random volatile-random volatile-ttl" && exit 1
+fi
+
+mkdir -p /usr/local/etc/redis
+F=/usr/local/etc/redis/redis.conf
+
+echo "maxmemory $1" > $F
+echo "maxmemory-policy $2" >> $F
+
+redis-server $F
+
